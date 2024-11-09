fb9b6668068b6506a600170550d876cd802840c6
Soniya <boralakuntasoniya@gmail.com>
2024-11-09 11:25:01 +0000

    project

---
+++ src/components/TrackListItem/index.js
@@ -0,0 +1,34 @@
+import {AiOutlineDelete} from 'react-icons/ai'
+import './index.css'
+
+const TrackListItem = props => {
+  const {initialTracksList, clickDlt} = props
+  const {id, imageUrl, name, genre, duration} = initialTracksList
+  const onClickDeleteIcon = () => {
+    clickDlt(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div className="first">
+        <img src={imageUrl} alt="track" className="li-img" />
+        <div className="name-container">
+          <p className='name'>{name}</p>
+          <p>{genre}</p>
+        </div>
+      </div>
+      <div className="second">
+        <p className="duration">{duration}</p>
+        <button
+          data-testid="delete"
+          className="dlt-btn"
+          onClick={onClickDeleteIcon}
+        >
+          <AiOutlineDelete className="dlt-icon" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default TrackListItem
